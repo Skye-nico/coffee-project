caae1aee59766ca488fb03c34e7d5e2fb361cf3f
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2021-11-13 23:27:00 -0600

    fixing code

--- main.js
+++ main.js
@@ -37,23 +37,17 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i < coffees.length - 1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
-function updateCoffees(e) {
-    console.log(search.value);
-    e.preventDefault(); // don't submit the form, we just want to update the data
+function updateCoffees() {
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        console.log(coffee.name)
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-        if (coffee.name.startsWith(search.value) === true) {
+        if ((coffee.roast === selectedRoast || coffee.all === selectedRoast) && coffee.name.toLowerCase().includes(search.value)) {
             filteredCoffees.push(coffee);
         }
     });
